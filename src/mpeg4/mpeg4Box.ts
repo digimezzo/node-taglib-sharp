d1f244bae06b8ed7bc3b0d24f98d3862ec960fcd
Digimezzo Raphaël <raphael@digimezzo.com>
2022-11-07 07:48:15 +0100

    Boxes are hard

--- src/mpeg4/mpeg4Box.ts
+++ src/mpeg4/mpeg4Box.ts
@@ -1,6 +1,10 @@
 import { ByteVector } from "../byteVector";
+import { File } from "../file";
+import { Guards } from "../utils";
+import IsoFreeSpaceBox from "./boxes/isoFreeSpaceBox";
 import IsoHandlerBox from "./boxes/isoHandlerBox";
 import Mpeg4BoxHeader from "./mpeg4BoxHeader";
+import Mpeg4BoxType from "./mpeg4BoxTypes";
 
 /**
  *  Provides a generic implementation of a ISO/IEC 14496-12 box.
@@ -87,10 +91,18 @@ export default class Mpeg4Box {
 
     /**
      * Gets the children of the current instance.
-     * @returns An array object enumerating the children of the current instance.
+     * @returns An Mpeg4Box[] object enumerating the children of the current instance.
      */
     public get children(): Mpeg4Box[] {
         return this._children;
+    }
+
+    /**
+     * Gets whether or not the current instance has children.
+     * @returns A  boolean value indicating whether or not the current instance has any children.
+     */
+    public get hasChildren(): boolean {
+        return this.children !== null && this.children !== undefined && this.children.length > 0;
     }
 
     /**
@@ -99,6 +111,30 @@ export default class Mpeg4Box {
      */
     public get handler(): IsoHandlerBox {
         return this._handler;
+    }
+
+    /**
+     * Gets the size of the data contained in the current instance, minus the size of any box specific headers.
+     * @returns A number value containing the size of the data contained in the current instance.
+     */
+    public get dataSize(): number {
+        return this._header.dataSize + this._dataPosition - this.dataPosition;
+    }
+
+    /**
+     * Gets the position of the data contained in the current instance, after any box specific headers.
+     * @returns A number value containing the position of the data contained in the current instance.
+     */
+    public get dataPosition(): number {
+        return this._dataPosition;
+    }
+
+    /**
+     * Gets the header of the current instance.
+     * @returns A Mpeg4BoxHeader object containing the header of the current instance.
+     */
+    public get header(): Mpeg4BoxHeader {
+        return this._header;
     }
 
     /**
@@ -239,9 +275,91 @@ export default class Mpeg4Box {
         this._children = [];
     }
 
-    public renderUsingTopData(topData: ByteVector): ByteVector {
-        // TODO
+    /**
+     * Loads the children of the current instance from a specified file using the internal data position and size.
+     * @param file The File from which the current instance was read and from which to read the children.
+     * @returns A Mpeg4Box[] object enumerating the boxes read from the file.
+     */
+    public loadChildren(file: File): Mpeg4Box[] {
+        Guards.notNullOrUndefined(file, "file");
 
-        return undefined;
+        const children: Mpeg4Box[] = [];
+
+        let position: number = this.dataPosition;
+        const end: number = position + this.dataSize;
+
+        this._header.box = this;
+
+        while (position < end) {
+            const child: Mpeg4Box = Mpeg4BoxFactory.CreateBox(file, position, this._header, this.handler, children.length);
+
+            if (child.size === 0) {
+                break;
+            }
+
+            children.push(child);
+            position += child.size;
+        }
+
+        this._header.box = undefined;
+
+        return children;
+    }
+
+    /**
+     * Loads the data of the current instance from a specified file using the internal data position and size.
+     * @param file The File from which the current instance was read and from which to read the data.
+     * @returns A ByteVector object containing the data read from the file.
+     */
+    public loadData(file: File): ByteVector {
+        Guards.notNullOrUndefined(file, "file");
+
+        file.seek(this.dataPosition);
+
+        return file.readBlock(this.dataSize);
+    }
+
+    /**
+     *
+     * @param topData
+     * @returns
+     */
+    public renderUsingTopData(topData: ByteVector): ByteVector {
+        let freeFound = false;
+        const output: ByteVector = ByteVector.empty();
+
+        if (this.children !== null && this.children !== undefined) {
+            for (const box of this.children) {
+                if (box instanceof IsoFreeSpaceBox) {
+                    freeFound = true;
+                } else {
+                    output.addByteVector(box.render());
+                }
+            }
+        } else if (this.data !== null && this.data !== undefined) {
+            output.addByteVector(this.data);
+        }
+
+        // If there was a free, don't take it away, and let meta be a special case.
+        if (freeFound || this.boxType === Mpeg4BoxType.meta) {
+            const sizeDifference: number = this.dataSize - output.length;
+
+            if (this._header.dataSize !== 0 && sizeDifference >= 8) {
+                // If we have room for free space, add it so we don't have to resize the file.
+                output.push(new IsoFreeSpaceBox(sizeDifference).render());
+            } else {
+                // If we're getting bigger, get a lot bigger so we might not have to again.
+                output.push(new IsoFreeSpaceBox(2048).render());
+            }
+        }
+
+        // Adjust the header's data size to match the content.
+        this._header.dataSize = topData.length + output.length;
+
+        // Render the full box.
+        output.insert(0, topData);
+        output.insert(0, this._header.render());
+
+        return output;
     }
 }

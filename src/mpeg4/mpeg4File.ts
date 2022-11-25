682a92e948cae99efa7be95b91da66414dd94863
Digimezzo <digimezzo@outlook.com>
2022-11-25 08:09:03 +0100

    Does this mean we can read now?

--- src/mpeg4/mpeg4File.ts
+++ src/mpeg4/mpeg4File.ts
@@ -2,7 +2,12 @@ import { File, FileAccessMode, ReadStyle } from "../file";
 import { IFileAbstraction } from "../fileAbstraction";
 import { Properties } from "../properties";
 import { Tag, TagTypes } from "../tag";
+import { AppleTag } from "./appleTag";
+import { IsoAudioSampleEntry } from "./boxes/isoAudioSampleEntry";
+import { IsoMovieHeaderBox } from "./boxes/isoMovieHeaderBox";
 import { IsoUserDataBox } from "./boxes/isoUserDataBox";
+import { IsoVisualSampleEntry } from "./boxes/isoVisualSampleEntry";
+import { Mpeg4BoxType } from "./mpeg4BoxType";
 import { Mpeg4FileParser } from "./mpeg4FileParser";
 import { Mpeg4Tag } from "./mpeg4Tag";
 
@@ -10,14 +15,44 @@ import { Mpeg4Tag } from "./mpeg4Tag";
  * Provides tagging and properties support for Mpeg4 files.
  */
 export class Mpeg4File extends File {
-    private readonly _properties: Properties;
+    /**
+     * Contains the Apple tag.
+     */
+    private _apple_tag: AppleTag;
+
+    /**
+     * Contains the combined tag.
+     */
+    private _tag: Mpeg4Tag;
+
+    /**
+     * Contains the media properties.
+     */
+    private _properties: Properties;
+
+    /**
+     * Contains the UDTA Boxes
+     */
     private readonly _udtaBoxes: IsoUserDataBox[] = [];
-    private readonly _tag: Mpeg4Tag;
+
+    /**
+     * The position at which the invariant portion of the current instance begins.
+     */
+    private _invariantStartPosition: number = -1;
+
+    /**
+     * The position at which the invariant portion of the current instance ends.
+     */
+    private _invariantEndPosition: number = -1;
 
     /** @inheritDoc */
     public constructor(file: IFileAbstraction | string, readStyle: ReadStyle) {
         super(file);
 
+        this.read(readStyle);
+    }
+
+    private read(readStyle: ReadStyle): void {
         this._tag = new Mpeg4Tag();
         this.mode = FileAccessMode.Read;
 
@@ -30,6 +65,54 @@ export class Mpeg4File extends File {
             } else {
                 parser.parseTagAndProperties();
             }
+
+            this._invariantStartPosition = parser.mdatStartPosition;
+            this._invariantEndPosition = parser.mdatEndPosition;
+
+            this._udtaBoxes.push(...parser.userDataBoxes);
+
+            // Ensure our collection contains at least a single empty box
+            if (this._udtaBoxes.length === 0) {
+                const dummy = new IsoUserDataBox();
+                this._udtaBoxes.push(dummy);
+            }
+
+            // Check if a udta with ILST actually exists
+            if (this.isAppleTagUdtaPresent()) {
+                this.tagTypesOnDisk |= TagTypes.Apple; // There is an udta present with ILST info
+            }
+
+            // Find the udta box with the Apple Tag ILST
+            let udtaBox: IsoUserDataBox = this.findAppleTagUdta();
+            if (udtaBox === null || udtaBox === undefined) {
+                udtaBox = new IsoUserDataBox();
+            }
+
+            this._apple_tag = new AppleTag(udtaBox);
+            this._tag.addTag(this._apple_tag);
+
+            // If we're not reading properties, we're done.
+            // TODO: was this check converted correctly?
+            if (readStyle === ReadStyle.Average) {
+                this.mode = FileAccessMode.Closed;
+
+                return;
+            }
+
+            // Get the movie header box.
+            const mvhd_box: IsoMovieHeaderBox = parser.movieHeaderBox;
+
+            if (mvhd_box === null || mvhd_box === undefined) {
+                this.mode = FileAccessMode.Closed;
+                throw new Error("mvhd box not found.");
+            }
+
+            const audio_sample_entry: IsoAudioSampleEntry = parser.audioSampleEntry;
+            const visual_sample_entry: IsoVisualSampleEntry = parser.visualSampleEntry;
+
+            // Read the properties.
+            // TODO: is this usage of the Properties constructor correct?
+            this._properties = new Properties(mvhd_box.durationInMilliseconds, [audio_sample_entry, visual_sample_entry]);
 
             // TODO
         } finally {
@@ -65,6 +148,57 @@ export class Mpeg4File extends File {
     /** @inheritDoc */
     public save(): void {
         // TODO
+    }
+
+    /**
+     * Gets the position at which the invariant portion of the current instance begins.
+     */
+    public get invariantStartPosition(): number {
+        return this._invariantStartPosition;
+    }
+
+    public get invariantEndPosition(): number {
+        return this._invariantEndPosition;
+    }
+
+    /**
+     * Find the udta box within our collection that contains the Apple ILST data.
+     * @returns The udta box within our collection that contains the Apple ILST data.
+     */
+    private findAppleTagUdta(): IsoUserDataBox {
+        if (this.udtaBoxes.length === 1) {
+            return this.udtaBoxes[0]; // Single udta - just return it
+        }
+
+        // multiple udta : pick out the shallowest node which has an ILst tag
+        // TODO: getChildRecursively should only return undefined and not null. Is this undefined check sufficient?
+        const possibleUdtaBoxes: IsoUserDataBox[] = this.udtaBoxes
+            .filter((box) => box.getChildRecursively(Mpeg4BoxType.Ilst) !== undefined)
+            .sort((box1, box2) => (box1.parentTree.length < box2.parentTree.length ? -1 : 1));
+
+        if (possibleUdtaBoxes.length > 0) {
+            return possibleUdtaBoxes[0];
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Gets if there is a udta with ILST present in our collection
+     * @returns True if there is a udta with ILST present in our collection
+     */
+    private isAppleTagUdtaPresent(): boolean {
+        for (const udtaBox of this._udtaBoxes) {
+            if (
+                udtaBox.getChild(Mpeg4BoxType.Meta) !== null &&
+                udtaBox.getChild(Mpeg4BoxType.Meta) !== undefined &&
+                udtaBox.getChild(Mpeg4BoxType.Meta).getChild(Mpeg4BoxType.Ilst) !== null &&
+                udtaBox.getChild(Mpeg4BoxType.Meta).getChild(Mpeg4BoxType.Ilst) !== undefined
+            ) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 

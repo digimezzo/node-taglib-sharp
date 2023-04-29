a57775ecd8bf71b22f3fe8e7d2a6757300b9f65c
Digimezzo <digimezzo@outlook.com>
2023-04-29 20:59:00 +0200

    mpeg4Utils are unit tested

--- test-unit/mpeg4/mpeg4UtilsTests.ts
+++ test-unit/mpeg4/mpeg4UtilsTests.ts
@@ -1,6 +1,7 @@
 import { suite, test } from "@testdeck/mocha";
 import { assert } from "chai";
 import { ByteVector } from "../../src/byteVector";
+import Mpeg4BoxHeader from "../../src/mpeg4/mpeg4BoxHeader";
 import Mpeg4Utils from "../../src/mpeg4/mpeg4Utils";
 
 @suite
@@ -31,5 +32,49 @@ class Mpeg4_Mpeg4UtilsTests {
 
         // Act/Assert
         assert.isUndefined(Mpeg4Utils.fixId(testByteVector));
+    }
+
+    @test
+    public addParent_withParentsNull_returnsListContainingOnlyCurrent() {
+        // Arrange
+        const current: Mpeg4BoxHeader = Mpeg4BoxHeader.fromEmpty();
+
+        // Act
+        const headers: Mpeg4BoxHeader[] = Mpeg4Utils.addParent(null, current);
+
+        // Assert
+        assert.equal(headers.length, 1);
+        assert.equal(headers[0], current);
+    }
+
+    @test
+    public addParent_withParentsUndefined_returnsListContainingOnlyCurrent() {
+        // Arrange
+        const current: Mpeg4BoxHeader = Mpeg4BoxHeader.fromEmpty();
+
+        // Act
+        const headers: Mpeg4BoxHeader[] = Mpeg4Utils.addParent(undefined, current);
+
+        // Assert
+        assert.equal(headers.length, 1);
+        assert.equal(headers[0], current);
+    }
+
+    @test
+    public addParent_withParentsHaving2Elements_returnsListContainingParentsFollowedByCurrent() {
+        // Arrange
+        const parent1: Mpeg4BoxHeader = Mpeg4BoxHeader.fromEmpty();
+        const parent2: Mpeg4BoxHeader = Mpeg4BoxHeader.fromEmpty();
+        const parents: Mpeg4BoxHeader[] = [parent1, parent2];
+        const current: Mpeg4BoxHeader = Mpeg4BoxHeader.fromEmpty();
+
+        // Act
+        const headers: Mpeg4BoxHeader[] = Mpeg4Utils.addParent(parents, current);
+
+        // Assert
+        assert.equal(headers.length, 3);
+        assert.equal(headers[0], parent1);
+        assert.equal(headers[1], parent2);
+        assert.equal(headers[2], current);
     }
 }
